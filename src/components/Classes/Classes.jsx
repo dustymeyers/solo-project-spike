151e407fd879a68f9de025a23c412e0e0bfd8880
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-03-14 21:31:16 -0500

    classes pull info from server

--- src/components/Classes/Classes.jsx
+++ src/components/Classes/Classes.jsx
@@ -9,6 +9,7 @@ function Classes() {
 
   const [classesList, setClassesList] = useState([]);
   const [chosenClass, setChosenClass] = useState(0);
+  const [classInfo, setClassInfo] = useState([]);
 
   console.log(classesList);
 
@@ -25,7 +26,14 @@ function Classes() {
 
   const chooseClass = () => {
     console.log(chosenClass);
-    
+    axios
+      .get(`/api/classes/${chosenClass}`)
+      .then(results => {
+        console.log('Class results are', results);
+
+        setClassInfo(results.data);
+      })
+      .catch(err => console.log('error getting class info', err));
   }
 
   return (
@@ -40,6 +48,16 @@ function Classes() {
           }) : <option>No classes</option>}
       </select>
       <button onClick={chooseClass}>Select</button>
+      <ul>
+      {classInfo.length ?
+        classInfo.map(info => {
+          return(
+            <li>{info.feature_name}: {info.feature_description}</li>
+          );
+        }) :
+        <p>Pick a class to get info</p>
+      }
+      </ul>
     </>
   )
 } // end Classes
